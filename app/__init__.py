6ecaed483da883d9a425f6276077a858e912b2f0
dondrzzy <sibo.donald16@gmail.com>
2018-02-22 15:33:33 +0300

    Refactoring Travis

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #Configuration Parameters for the app
 app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:engneerdon@localhost/tbc_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =  = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']  = False
 db = SQLAlchemy(app)
 
 from app.schemas import User, Business, Review
