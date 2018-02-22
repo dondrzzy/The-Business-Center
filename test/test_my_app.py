955386daf121a68c41d0e5a0665fc81104c61c5f
dondrzzy <sibo.donald16@gmail.com>
2018-02-22 16:37:45 +0300

    Refactoring Travis

--- test/test_my_app.py
+++ test/test_my_app.py
@@ -1,17 +1,20 @@
 import unittest
-from app.models.user import UserModel
+from app.controllers.business_controller import BusinessController
+from app.controllers.user_controller import UserController
+
  
 class TestApp(unittest.TestCase):
-	def setUp(self):
-		self.user = UserModel()
+    def setUp(self):
+        self.user_controller = UserController()
 
-	def test_model_is_member(self):
-		res = self.user.is_member({"email":"no_email"})
-		self.assertEqual(res["success"], False, msg="Is Member function is not working")
-
-	def test_model_reset_password(self):
-		self.assertFalse(self.user.reset_password({"email":"123"})["success"], msg="Method should return false")
+    def test_user_registration_no_name(self):
+        test_user={
+            "email":"test@gmail.com",
+            "password":"1234",
+            "password_c":"1234"
+        }
+        self.assertEqual(self.user_controller.register_user(test_user), {"success":False, "msg":"name is required"}, msg="Registration method faulty")
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
