0409b9bb99fa7c7cbaa76042e2225faf385509ec
dondrzzy <sibo.donald16@gmail.com>
2018-02-22 11:23:03 +0300

    Modified a few tests after intergrating postman

--- test/__init__.py
+++ test/__init__.py
@@ -1,3 +1,3 @@
 import os, sys
 CURRENT_DIR = os.path.dirname(os.path.abspath('__init__.py'))
-sys.path.append(os.path.dirname(CURRENT_DIR+'\\models'))
+sys.path.append(os.path.dirname(CURRENT_DIR+'\\app'))
