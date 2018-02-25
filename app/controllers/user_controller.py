33a74d601b2a06df5970ce053a24e0dd8f1bdcea
dondrzzy <sibo.donald16@gmail.com>
2018-02-25 04:18:39 +0300

    Refactoring code climate badge

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -12,7 +12,6 @@ class UserController(object):
 
     def register_user(self, data):
         """register_user"""
-
         if "name" not in data:
             return {"success":False, "msg":"name is required"}
         elif "email" not in data:
@@ -23,13 +22,10 @@ class UserController(object):
             return {"success":False, "msg":"password_c is required"}
         elif data["password"] != data["password_c"]:
             return {"success":False, "msg":"passwords do not match"}
-
         user_obj = {
-            "name" : data["name"],
-            "email" : data["email"],
+            "name" : data["name"], "email" : data["email"],
             "password" : data["password"]
         }
-
         return UM.register_user(user_obj)
 
     # login user
@@ -39,14 +35,9 @@ class UserController(object):
             return {"success":False, "msg":"Email is required"}
         if "password" not in data:
             return {"success":False, "msg":"Password is required"}
-
         user_obj = {
-            "email" : data["email"],
-            "password" : data["password"]
+            "email" : data["email"], "password" : data["password"]
         }
-
-
-        # check if already logged in user
         if "email" in session and session["email"] == data["email"]:
             return {"success":False, "msg":"Already logged in. Redirecting..."}
 
@@ -65,9 +56,6 @@ class UserController(object):
             return {"success":False, "msg":"passwords do not match"}
 
         user_obj = {
-            "email" : data["email"],
-
-            "password" : (data["password"])
+            "email" : data["email"], "password" : (data["password"])
         }
-
         return UM.reset_password(user_obj)
